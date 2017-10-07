25ccbff1c638c5a719209248236f213615d4e1ed
Ryan Tanay <ryantanay@newevolution.org>
2017-10-07 16:21:57 -0500

    Created models
    Using Entity Framework code-first migrations, created linked models for all DB objects

---
+++ horse_haven_dotnet/src/horse_haven_dotnet/Models/BoardingType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace horse_haven_dotnet.Models
+{
+    public class BoardingType
+    {
+        public int BoardingTypeId { get; set; }
+        public decimal DailyRate { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ horse_haven_dotnet/src/horse_haven_dotnet/Models/Horse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace horse_haven_dotnet.Models
+{
+    public class Horse
+    {
+        public int HorseId { get; set; }
+        public string HorseHavenId { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string CountyOfOrigin { get; set; }
+
+        public int HorseStatusId { get; set; }
+        public HorseStatus HorseStatus { get; set; }
+
+        public DateTime ArrivalDate { get; set; } = DateTime.UtcNow;
+        public DateTime DepartureDate { get; set; }
+        public DateTime EligibleForOwnership { get; set; }
+
+        public int AdopterId { get; set; }
+        [ForeignKey("AdopterId")]
+        public Person Person { get; set; }
+
+        public int CaseId { get; set; }
+        public Case Case { get; set; }
+    }
+}
