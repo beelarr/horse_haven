a069cf1e420bdfb79c1870c53efc51ea2c041a2c
Ryan Tanay <ryantanay@newevolution.org>
2017-10-07 22:21:59 -0500

    Renaming, and horse controller

--- horse_haven_dotnet/horse_haven_dotnet/Controllers/BoardingsController.cs
+++ horse_haven_dotnet/horse_haven_dotnet/Controllers/BoardingsController.cs
@@ -11,43 +11,48 @@ using Microsoft.EntityFrameworkCore;
 namespace horse_haven_dotnet.Controllers
 {
     [Route("api/[controller]")]
-    public class BoardingController : Controller
+    public class BoardingsController : Controller
     {
         private WebAPIDataContext _webAPIDataContext;
-        public BoardingController(WebAPIDataContext webAPIDataContext)
+        public BoardingsController(WebAPIDataContext webAPIDataContext)
         {
             _webAPIDataContext = webAPIDataContext;
         }
 
-        // GET: api/boarding
+        // GET: api/boardings
         [HttpGet]
         public IEnumerable<Boarding> Get()
         {
             return _webAPIDataContext.Boardings.AsEnumerable();
         }
 
-        // GET api/boarding/5
+        // GET api/boardings/5
         [HttpGet("{id}")]
         public Boarding Get(int id)
         {
             return _webAPIDataContext.Boardings.FirstOrDefault(x => x.BoardingId == id);
         }
 
-        // GET api/boarding/type/5
+        // GET api/boardings/type/5
         [HttpGet("/type/{id}")]
         public IEnumerable<Boarding> GetByType(int id)
         {
             return _webAPIDataContext.Boardings.Where(x => x.BoardingTypeId == id);
         }
 
-        // GET api/boarding/date/7/2017
-        [HttpGet("/date/{month}/{year}")]
+        // GET api/boardings/active_during_month/7/2017
+        [HttpGet("/active_during_month/{month}/{year}")]
         public IEnumerable<Boarding> GetByDate(int month, int year)
         {
-            return _webAPIDataContext.Boardings.Where(x => x.StartDate.Month <= month && x.EndDate.Month >= month && x.StartDate.Year == year);
+            return _webAPIDataContext.Boardings.Where
+                (
+                    x => x.StartDate.Year == year
+                    && (x.EndDate == null || x.EndDate.Month >= month)
+                    && x.StartDate.Month <= month
+                );
         }
 
-        // POST api/boarding
+        // POST api/boardings
         [HttpPost]
         public void Post([FromBody]Boarding boarding)
         {
@@ -55,7 +60,7 @@ namespace horse_haven_dotnet.Controllers
             _webAPIDataContext.SaveChangesAsync();
         }
 
-        // PUT api/boarding/5
+        // PUT api/boardings/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]Boarding boarding)
         {
@@ -67,7 +72,7 @@ namespace horse_haven_dotnet.Controllers
             }
         }
 
-        // DELETE api/boarding/5
+        // DELETE api/boardings/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
