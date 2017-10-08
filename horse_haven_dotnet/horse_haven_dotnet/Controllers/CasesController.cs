008aa615db34ac034eefe5ca13608119d75d7e4b
Ryan Tanay <ryantanay@newevolution.org>
2017-10-07 23:00:10 -0500

    Case, service and service type controllers

---
+++ horse_haven_dotnet/horse_haven_dotnet/Controllers/CasesController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using horse_haven_dotnet.Models;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace horse_haven_dotnet.Controllers
+{
+    [Route("api/[controller]")]
+    public class CasesController : Controller
+    {
+        private WebAPIDataContext _webAPIDataContext;
+        public CasesController(WebAPIDataContext webAPIDataContext)
+        {
+            _webAPIDataContext = webAPIDataContext;
+        }
+
+        // GET: api/cases
+        [HttpGet]
+        public IEnumerable<Case> Get()
+        {
+            return _webAPIDataContext.Cases.AsEnumerable();
+        }
+
+        // GET api/cases/5
+        [HttpGet("{id}")]
+        public Case Get(int id)
+        {
+            return _webAPIDataContext.Cases.FirstOrDefault(x => x.CaseId == id);
+        }
+
+        // POST api/cases
+        [HttpPost]
+        public void Post([FromBody]Case boarding)
+        {
+            _webAPIDataContext.Add(boarding);
+            _webAPIDataContext.SaveChangesAsync();
+        }
+
+        // PUT api/cases/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Case boarding)
+        {
+            var selectedCase = _webAPIDataContext.Cases.AsNoTracking().FirstOrDefaultAsync(x => x.CaseId == id);
+            if (selectedCase != null)
+            {
+                _webAPIDataContext.Entry(selectedCase).Context.Update(boarding);
+                _webAPIDataContext.SaveChangesAsync();
+            }
+        }
+
+        // DELETE api/cases/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var boarding = _webAPIDataContext.Cases.FirstOrDefault(x => x.CaseId == id);
+            if (boarding != null)
+            {
+                _webAPIDataContext.Cases.Remove(boarding);
+                _webAPIDataContext.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ horse_haven_dotnet/horse_haven_dotnet/Controllers/ServiceTypesController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using horse_haven_dotnet.Models;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace horse_haven_dotnet.Controllers
+{
+    [Route("api/[controller]")]
+    public class ServiceTypesController : Controller
+    {
+        private WebAPIDataContext _webAPIDataContext;
+        public ServiceTypesController(WebAPIDataContext webAPIDataContext)
+        {
+            _webAPIDataContext = webAPIDataContext;
+        }
+
+        // GET: api/servicetypes
+        [HttpGet]
+        public IEnumerable<ServiceType> Get()
+        {
+            return _webAPIDataContext.ServiceTypes.AsEnumerable();
+        }
+
+        // GET api/servicetypes/5
+        [HttpGet("{id}")]
+        public ServiceType Get(int id)
+        {
+            return _webAPIDataContext.ServiceTypes.FirstOrDefault(x => x.ServiceTypeId == id);
+        }
+
+        // POST api/servicetypes
+        [HttpPost]
+        public void Post([FromBody]ServiceType boarding)
+        {
+            _webAPIDataContext.Add(boarding);
+            _webAPIDataContext.SaveChangesAsync();
+        }
+
+        // PUT api/servicetypes/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]ServiceType boarding)
+        {
+            var selectedServiceType = _webAPIDataContext.ServiceTypes.AsNoTracking().FirstOrDefaultAsync(x => x.ServiceTypeId == id);
+            if (selectedServiceType != null)
+            {
+                _webAPIDataContext.Entry(selectedServiceType).Context.Update(boarding);
+                _webAPIDataContext.SaveChangesAsync();
+            }
+        }
+
+        // DELETE api/servicetypes/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var boarding = _webAPIDataContext.ServiceTypes.FirstOrDefault(x => x.ServiceTypeId == id);
+            if (boarding != null)
+            {
+                _webAPIDataContext.ServiceTypes.Remove(boarding);
+                _webAPIDataContext.SaveChangesAsync();
+            }
+        }
+    }
+}
